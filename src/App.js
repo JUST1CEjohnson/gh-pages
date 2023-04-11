497e9e5744ba7ae55cf6625b471b0b08c7486d68
JJ <continuouscloudconsulting@gmail.com>
2023-04-11 16:47:00 -0400

    updated descriptions

--- src/App.js
+++ src/App.js
@@ -11,6 +11,11 @@ function App() {
         </p>
         <p>Technical consulting company that specalizes in kubernetes and DevOps principles.</p>
         <p>Please contact continuouscloudconsulting@gmail.com for all inquiries.</p>
+        <p>Continuous Cloud provides consulting services for small businesses, and offers a variety of services from building out core infrastructure to developing software and solutions
+          for a company that wants to migrate to the cloud. What we offer here is technical expertise and years of enterprise knowledge. Services will vary per client but our focus is primarily on 
+          kubernetes and containerization, although we are experiencedin traditional infrastructure as well. Please feel free to contact us with any questions you may have or for anything 
+          you would like to know more about. Rates will vary per project and can be discussed via e-mail.
+        </p>
       </header>
       <footer><p>Copyright Continuous Cloud LLC Jacksonville, FL</p></footer>
     </div>
