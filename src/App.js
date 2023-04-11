5ea3489bdd6af9bc462eeb50730e1f802866537a
JJ <continuouscloudconsulting@gmail.com>
2023-04-11 09:02:59 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -9,14 +9,7 @@ function App() {
         <p>
           Continuous Cloud Consulting
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Jacksonville, FL
       </header>
     </div>
   );
